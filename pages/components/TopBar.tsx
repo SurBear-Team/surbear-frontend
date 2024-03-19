7ddee933243e8d749791346d052a873d1a718a4d
kimgorok <kjyse2@naver.com>
2024-03-19 22:20:45 +0900

    🎨탑바_중앙에 배치

--- pages/components/TopBar.tsx
+++ pages/components/TopBar.tsx
@@ -5,7 +5,7 @@ interface TopBarProps {
 
 export const TopBar: React.FC<TopBarProps> = ({ onClick, leftSVG }) => {
   return (
-    <div className="bg-white fixed w-full">
+    <div className="bg-white left-0 right-0 mx-auto fixed w-full max-w-[36rem]  justify-center">
       <div className="flex gap-2 pl-6 py-3 ">
         <div onClick={onClick}>{leftSVG}</div>
         <div className="font-extrabold">회원가입</div>

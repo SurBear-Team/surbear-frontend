90a1ed1b2edfe751815e9b9220c7add992ea4473
kimgorok <kjyse2@naver.com>
2024-03-20 13:41:32 +0900

    🔮회원가입_아이디 비번 입력에 useForm 추가

--- pages/sign-up/index.tsx
+++ pages/sign-up/index.tsx
@@ -1,53 +1,119 @@
 import { useRouter } from "next/router";
 import { TopBar } from "../components/TopBar";
+import { useForm } from "react-hook-form";
 
 export default function IdPassword() {
   const router = useRouter();
+
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm({
+    mode: "onChange",
+  });
+
+  const onSubmit = (data: any) => console.log(data);
+
+  const validateUsername = (username: string) => {
+    // 유효성검사
+    const isValidContent = /^[A-Za-z0-9]+$/.test(username);
+
+    // 중복 검사 일단 true로
+    const isUnique = true;
+    return isValidContent && isUnique;
+  };
 
   return (
     <>
       <TopBar onClick={() => router.back()} leftSVG={"⬅"} />
       <div className="screen px-12 flex-col w-full">
-        {/* 전화번호 */}
-        <div className="w-full">
-          <span className="font-extrabold">아이디</span>
-          <input
-            type="text"
-            placeholder="아이디를 입력해주세요"
-            className="main-input mt-1 mb-10 text-[#101010]"
-          />
-        </div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          {/* 아이디 */}
+          <div className="w-full relative">
+            <span className="font-extrabold">아이디</span>
+            <input
+              {...register("username", {
+                validate: validateUsername,
+                required: "아이디를 입력해주세요.",
+                minLength: {
+                  value: 6,
+                  message: "아이디는 최소 6자 이상이어야 합니다.",
+                },
+              })}
+              type="text"
+              placeholder="아이디를 입력해주세요"
+              className={`main-input mt-1 mb-10 text-[#101010] ${
+                errors.username ? "border-red-500" : ""
+              }`}
+            />
+            {errors.username && typeof errors.username.message === "string" ? (
+              <p className="absolute bottom-4 font-extrabold text-xs text-[#D52828]">
+                {errors.username.message}
+              </p>
+            ) : null}
+          </div>
 
-        {/* 비밀번호 */}
-        <div className="w-full">
-          <span className="font-extrabold ">비밀번호</span>
-          <input
-            type="password"
-            placeholder="비밀번호를 입력해주세요"
-            className="main-input mt-1 mb-10 text-[#101010] "
-          />
-        </div>
+          {/* 비밀번호 */}
+          <div className="w-full relative">
+            <span className="font-extrabold ">비밀번호</span>
+            <input
+              {...register("password", {
+                required: "비밀번호를 입력해주세요.",
+                minLength: {
+                  value: 8,
+                  message: "비밀번호는 최소 8자 이상이어야 합니다.",
+                },
+              })}
+              type="password"
+              placeholder="비밀번호를 입력해주세요"
+              className={`main-input mt-1 mb-10 text-[#101010] ${
+                errors.username ? "border-red-500" : ""
+              }`}
+            />
+            {errors.password && typeof errors.password.message === "string" ? (
+              <p className="absolute bottom-4 font-extrabold text-xs text-[#D52828]">
+                {errors.password.message}
+              </p>
+            ) : null}
+          </div>
 
-        {/* 비밀번호 확인 */}
-        <div className="w-full">
-          <span className="font-extrabold mb-1">비밀번호 확인</span>
-          <input
-            type="password"
-            placeholder="비밀번호를 확인해주세요"
-            className="main-input mt-1 mb-10 text-[#101010]"
-          />
-        </div>
+          {/* 비밀번호 확인 */}
+          <div className="w-full relative">
+            <span className="font-extrabold mb-1">비밀번호 확인</span>
+            <input
+              {...register("passwordConfirm", {
+                validate: (value) =>
+                  value === watch("password") ||
+                  "비밀번호가 일치하지 않습니다.",
+              })}
+              type="password"
+              placeholder="비밀번호를 확인해주세요"
+              className={`main-input mt-1 mb-10 text-[#101010] ${
+                errors.username ? "border-red-500" : ""
+              }`}
+            />
+            {errors.passwordConfirm &&
+            typeof errors.passwordConfirm.message === "string" ? (
+              <p className="absolute bottom-4 font-extrabold text-xs text-[#D52828]">
+                {errors.passwordConfirm.message}
+              </p>
+            ) : null}
+          </div>
 
-        <div className="gray-line w-full mt-12 mx-10" />
+          <div className="gray-line w-full mt-12 mx-10" />
 
-        <div className="w-full">
-          <button
-            className="long-button px-32 mt-8 font-extrabold bg-white border-[#6E7CF2] text-[#6E7CF2]"
-            onClick={() => {}}
-          >
-            다음
-          </button>
-        </div>
+          <div className="w-full">
+            <button
+              type="submit"
+              className="long-button px-32 mt-8 font-extrabold bg-white border-[#6E7CF2] text-[#6E7CF2]"
+              onClick={() => {}}
+            >
+              다음
+            </button>
+          </div>
+        </form>
       </div>
     </>
   );

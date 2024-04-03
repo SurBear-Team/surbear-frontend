523a28aa73e5f168d1e7720563bef33ce7a842e1
kimgorok <kjyse2@naver.com>
2024-04-03 21:28:46 +0900

    🔮다이얼로그_텍스트에리어 옵션 추가

--- pages/components/Dialog.tsx
+++ pages/components/Dialog.tsx
@@ -1,5 +1,9 @@
+import { Overlay } from "./styles/Overlay";
+
 interface DialogProps {
   title: string;
+  hasTextarea?: boolean;
+  inputTitle?: string;
   leftText: string;
   onLeftClick: () => void;
   rightText: string;
@@ -9,6 +13,8 @@ interface DialogProps {
 
 export const Dialog = ({
   title,
+  hasTextarea,
+  inputTitle,
   leftText,
   onLeftClick,
   rightText,
@@ -16,26 +22,42 @@ export const Dialog = ({
   isDelete,
 }: DialogProps) => {
   return (
-    <div className="inline-flex flex-col justify-center items-start fixed px-8 py-6 gap-8 rounded-lg bg-white z-20">
-      <div className="text-gray-9 text-center font-semibold w-full">
-        {title}
+    <>
+      <Overlay />
+      <div className="inline-flex flex-col justify-center items-start fixed px-8 py-6 gap-8 rounded-lg bg-white z-20">
+        <div className="text-gray-9 text-center font-semibold w-full">
+          {title}
+        </div>
+        {/* 텍스트에리어 */}
+        {hasTextarea && (
+          <div className="w-full flex flex-col gap-2 -mt-4">
+            <div className="w-full sm-gray-9-text font-medium">
+              {inputTitle}
+            </div>
+            <textarea className="w-full p-2 items-start border-[1px] border-gray-4 sm-gray-9-text font-normal" />
+          </div>
+        )}
+
+        {/* 하단버튼두개 */}
+        <div className="flex justify-center items-center gap-4 w-full">
+          <button
+            onClick={onLeftClick}
+            className="medium-Btn border-gray-5 bg-white text-gray-5"
+          >
+            {leftText}
+          </button>
+          <button
+            onClick={onRightClick}
+            className={`medium-Btn text-white ${
+              isDelete
+                ? "border-red-1 bg-red-1"
+                : "border-primary-1 bg-primary-1"
+            }`}
+          >
+            {rightText}
+          </button>
+        </div>
       </div>
-      <div className="flex justify-center items-center gap-4 w-full">
-        <button
-          onClick={onLeftClick}
-          className="medium-Btn border-gray-5 bg-white text-gray-5"
-        >
-          {leftText}
-        </button>
-        <button
-          onClick={onRightClick}
-          className={`medium-Btn text-white ${
-            isDelete ? "border-red-1 bg-red-1" : "border-primary-1 bg-primary-1"
-          }`}
-        >
-          {rightText}
-        </button>
-      </div>
-    </div>
+    </>
   );
 };

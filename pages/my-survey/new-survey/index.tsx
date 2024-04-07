9e3af562888b844e7480150977c8f8cc49a4d1a8
kimgorok <kjyse2@naver.com>
2024-04-07 20:46:54 +0900

    🛠내 설문_CreatedQuestion 컴포넌트 생성

--- pages/my-survey/new-survey/index.tsx
+++ pages/my-survey/new-survey/index.tsx
@@ -3,6 +3,7 @@ import { useRouter } from "next/router";
 import { useState } from "react";
 import { MakeSurvey } from "./components/MakeSurvey";
 import { InputTopBar } from "@/pages/my-survey/new-survey/components/InputTopBar";
+import { CreatedQuestion } from "./components/CreatedQuestion";
 
 export default function NewSurvey() {
   const router = useRouter();
@@ -27,34 +28,11 @@ export default function NewSurvey() {
         }}
       />
       <div className="screen flex-col pt-14 justify-start">
-        <div className="px-6">
-          {/* 질문제목 */}
-          <div className="flex pb-6">
-            <div className="sm-gray-9-text text-base pr-2">{`1.`}</div>
-            <div className="sm-gray-9-text text-base">{`질문제목 4줄까지 질문제목 4줄까지 질문제목 4줄까지 질문제목 4줄까지 질문제목 4줄까지 질문제목 4줄까지 질문제목 4줄까지 질문제목 4줄까지 질문제목 4줄까지 질문제목 4줄까지 질문제목 4줄까지 질문제목 4줄까지 질문제목 4줄까지 질문제목 4줄까지 `}</div>
-          </div>
-
-          {/* 질문답변 */}
-          {MultipleList.map((index) => (
-            <div
-              key={index}
-              className="flex w-full p-2 mb-4 items-center gap-3 border-[1px] border-gray-4 rounded-lg"
-            >
-              <div className="check-box bg-white border-[1px] border-gray-7 min-w-4" />
-              <div className="sm-gray-9-text font-normal">{`선택지 2줄까지 선택지 2줄까지 선택지 2줄까지 선택지 2줄까지 선택지 2줄까지 선택지 2줄까지 선택지 2줄까지 선택지 2줄까지 `}</div>
-            </div>
-          ))}
-
-          {/* 수정 삭제 버튼 */}
-          <div className="flex gap-2 justify-end">
-            <button className="small-Btn white-bg-primary-btn">수정</button>
-            <button className="small-Btn border-red-1 bg-red-1 text-white">
-              삭제
-            </button>
-          </div>
-
-          <div className="gray-line mt-4 mb-10" />
-        </div>
+        <CreatedQuestion
+          AmswerIndex={1}
+          title={`제목`}
+          AnswerList={MultipleList}
+        />
 
         {surveyComponents.map((_, index) => (
           <MakeSurvey

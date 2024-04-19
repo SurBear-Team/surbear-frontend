202a4c0ba6a88efb4edd0161738f0aa26f094b4c
kimgorok <kjyse2@naver.com>
2024-04-19 22:02:18 +0900

    🐢새 설문_일단 설문 DELETE

--- pages/my-survey/index.tsx
+++ pages/my-survey/index.tsx
@@ -1,14 +1,55 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { TabBar } from "../components/TabBar";
 import { MySurveyCard } from "./components/MySurveyCard";
 import { Dialog } from "../components/Dialog";
 import { NewSurveyCard } from "./components/NewSurveyCard";
 import { TopBar } from "../components/TopBar/TopBar";
+import api from "../api/config";
+import { ISurvey } from "../browse/data";
+import axios from "axios";
+
+interface DialogState {
+  open: boolean;
+  text: string;
+  rightText: string;
+  isDelete: boolean;
+  surveyId: number; // 진짜 id 들어오면 string으로 바꿈
+}
+
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 
 export default function MySurvey() {
-  const [showDeleteDialog, setShowDeleteDialog] = useState(false);
-  const [showUpdateDialog, setShowUpdateDialog] = useState(false);
+  const [dialog, setDialog] = useState<DialogState>({
+    open: false,
+    text: "",
+    rightText: "",
+    isDelete: false,
+    surveyId: 0, // 이거
+  });
 
+  const [mySurveyList, setMySurveyList] = useState<ISurvey[]>();
+
+  useEffect(() => {
+    api
+      .get("/survey/management/list/3")
+      .then((res) => {
+        setMySurveyList(res.data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
+
+  const deleteSurvey = (id: number) => {
+    axios
+      .delete(`${baseUrl}/survey/management/${id}`)
+      .then(() => {
+        console.log(id + "삭제");
+        setMySurveyList((prev) => prev?.filter((survey) => survey.id !== id));
+        setDialog({ ...dialog, open: false });
+      })
+      .catch((err) => console.log(err));
+  };
+
+  // 새 설문 만들기
   const [showNewSurveyCard, setShowNewSurveyCard] = useState(false);
   return (
     <>
@@ -23,82 +64,59 @@ export default function MySurvey() {
       <div className="screen pt-[50px]">
         <div className="list-screen">
           <div className="list">
-            <MySurveyCard
-              category={`카테고리`}
-              title={`제목 2줄까지`}
-              onDeleteClick={() => {
-                setShowDeleteDialog(true);
-              }}
-              beforeStart={true}
-              onUpdateClick={() => {
-                setShowUpdateDialog(true);
-              }}
-              onBlueBtnClick={() => {
-                console.log("설문 시작");
-              }}
-            />
-
-            <MySurveyCard
-              category={`카테고리`}
-              title={`제목 2줄까지`}
-              onDeleteClick={() => {
-                setShowDeleteDialog(true);
-              }}
-              beforeFinish={true}
-              onUpdateClick={() => {
-                setShowUpdateDialog(true);
-              }}
-              onBlueBtnClick={() => {
-                console.log("설문 종료");
-              }}
-            />
-
-            <MySurveyCard
-              category={`카테고리`}
-              title={`제목 2줄까지`}
-              onDeleteClick={() => {
-                setShowDeleteDialog(true);
-              }}
-              showResult={true}
-              onBlueBtnClick={() => {
-                console.log("결과 보기");
-              }}
-            />
+            {mySurveyList?.map(
+              (data, index) =>
+                !data.deleted && ( // 삭제되지 않은거만 보이게
+                  <MySurveyCard
+                    key={index}
+                    category={data.surveyType}
+                    title={data.title}
+                    onDeleteClick={() => {
+                      setDialog({
+                        open: true,
+                        text: "설문을 삭제하시겠습니까?",
+                        rightText: "삭제",
+                        isDelete: true,
+                        surveyId: data.id,
+                      });
+                    }}
+                    beforeStart={true} // true면 설문 시작 나옴
+                    beforeFinish={false} // true면 설문 종료 나옴
+                    showResult={false} // true면 결과 보기 나옴
+                    onUpdateClick={() => {
+                      setDialog({
+                        open: true,
+                        text: "설문을 수정하시겠습니까?",
+                        rightText: "수정",
+                        isDelete: false,
+                        surveyId: data.id,
+                      });
+                    }}
+                    onBlueBtnClick={() => {
+                      console.log("설문 시작");
+                    }}
+                  />
+                )
+            )}
           </div>
         </div>
         {showNewSurveyCard && (
           <NewSurveyCard onCancel={() => setShowNewSurveyCard(false)} />
         )}
 
-        {showDeleteDialog && (
+        {dialog.open && (
           <>
             <Dialog
-              title="설문을 삭제하시겠습니까?"
+              title={dialog.text}
               leftText="취소"
-              onLeftClick={() => {
-                setShowDeleteDialog((prev) => !prev);
-              }}
-              rightText="삭제"
+              onLeftClick={() => setDialog({ ...dialog, open: false })}
+              rightText={dialog.rightText}
               onRightClick={() => {
-                console.log("`번호` 삭제");
+                if (dialog.isDelete && dialog.surveyId) {
+                  deleteSurvey(dialog.surveyId);
+                }
               }}
-              isDelete={true}
-            />
-          </>
-        )}
-
-        {showUpdateDialog && (
-          <>
-            <Dialog
-              title="설문을 수정하시겠습니까?"
-              leftText="취소"
-              onLeftClick={() => {
-                setShowUpdateDialog((prev) => !prev);
-              }}
-              rightText="수정"
-              onRightClick={() => {
-                console.log("`번호` 수정");
-              }}
+              isDelete={dialog.isDelete}
             />
           </>
         )}

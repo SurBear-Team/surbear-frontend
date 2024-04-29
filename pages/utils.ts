b2b99386e8f68119e49fece60aa357f143b83945
seorang42 <seorang42@naver.com>
2024-04-29 23:15:28 +0900

    회원 설문조사 조회 기능 구현

--- pages/utils.ts
+++ pages/utils.ts
@@ -1,13 +1,21 @@
 import dayjs from "dayjs";
 
-export const getTime = (time: string) => {
-  const dayJs = dayjs(time);
-  const year = dayJs.year();
-  const month = dayJs.month() + 1;
-  const date = dayJs.date();
-  const hour = dayJs.hour();
-  const minute = dayJs.minute();
-  return { year, month, date, hour, minute };
+export const getTime = (time: string, asString?: boolean) => {
+  if (time === null) {
+    return null;
+  } else {
+    const dayJs = dayjs(time);
+    const year = dayJs.year();
+    const month = dayJs.month() + 1;
+    const date = dayJs.date();
+    const hour = dayJs.hour();
+    const minute = dayJs.minute();
+    if (asString === true) {
+      return `${year}.${month}.${date}`;
+    } else {
+      return { year, month, date, hour, minute };
+    }
+  }
 };
 
 export const getAge = (age: string) => {

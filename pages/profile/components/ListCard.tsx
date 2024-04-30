9e96877c3ec078cac229c7ab81babfeee5e9292a
seorang42 <seorang42@naver.com>
2024-04-30 21:22:27 +0900

    회원 설문조사 참여 내역 기능 구현

--- pages/profile/components/ListCard.tsx
+++ pages/profile/components/ListCard.tsx
@@ -7,6 +7,7 @@ interface ListCardProps {
   point?: string;
   status?: string;
   hasEdit?: boolean;
+  surveyOwner?: string;
 }
 
 export const ListCard = ({
@@ -16,12 +17,17 @@ export const ListCard = ({
   point,
   status,
   hasEdit,
+  surveyOwner,
 }: ListCardProps) => {
   return (
-    <div className="w-full flex justify-between h-16 px-6 items-center bg-white border-b-[1px] border-gray-4">
-      <div className="text-gray-9 text-sm font-semibold">{getTime}</div>
-      <div className="flex items-center gap-2">
-        <div className="sm-gray-text">{content}</div>
+    <div className="w-full flex gap-2 justify-between h-16 px-6 items-center bg-white border-b-[1px] border-gray-4">
+      <div className="text-gray-9 text-sm font-semibold whitespace-nowrap">
+        {getTime !== "" ? getTime : "시작 전"}
+      </div>
+      <div className="flex items-center gap-2 flex-grow overflow-hidden">
+        <div className="w-full sm-gray-text text-ellipsis whitespace-nowrap overflow-hidden">
+          {content}
+        </div>
         {plusMinus && point && (
           <div className="text-gray-9 font-semibold">
             {plusMinus}
@@ -30,17 +36,29 @@ export const ListCard = ({
         )}
         {status && (
           <div
-            className={`font-medium text-xs ${
+            className={`font-medium text-xs text-gray-9 whitespace-nowrap ${
               status === "PROGRESS" && "text-primary-1"
+            } ${
+              status === "CLOSE" ||
+              status === "DELETION" ||
+              (status === "FORCED_DELETION" && "text-red-1")
             }`}
           >
             {status === "PAUSE" && "시작 전"}
             {status === "PROGRESS" && "진행중"}
+            {status === "CLOSE" && "종료됨"}
+            {status === "DELETION" && "삭제됨"}
+            {status === "FORCED_DELETION" && "강제 삭제됨"}
           </div>
         )}
         {hasEdit && (
           <div className="cursor-pointer" onClick={() => {}}>
             <SettingIcon isSmall />
+          </div>
+        )}
+        {surveyOwner && (
+          <div className="flex items-center font-medium text-gray-9 text-xs whitespace-nowrap">
+            {surveyOwner}
           </div>
         )}
       </div>

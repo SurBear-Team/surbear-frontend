ae86ae9d1584cf3abd5e448d82bdfd6a4d092430
seorang42 <seorang42@naver.com>
2024-05-02 16:02:55 +0900

    회원정보 조회 및 수정 기능 구현

--- pages/profile/setting/update.tsx
+++ pages/profile/setting/update.tsx
@@ -6,18 +6,21 @@ import { AgeSheet } from "@/pages/sign-up/Components/AgeSheet";
 import { TopBar } from "@/pages/components/TopBar/TopBar";
 import api from "@/pages/api/config";
 import { IMemberInfo } from "@/pages/manager/member";
-import { getAge } from "@/pages/utils";
+import { getAge, getConstAge } from "@/pages/utils";
 import { InputDialog } from "@/pages/manager/components/InputDialog";
 
 export default function MemberUpdate() {
   const router = useRouter();
+  const [updateList, setUpdateList] = useState(0);
 
+  const [token, setToken] = useState("");
   useEffect(() => {
     if (typeof window !== undefined) {
       const checkToken = localStorage.getItem("surbearToken");
       if (checkToken === null) {
         router.push("/sign-in");
       } else {
+        setToken(checkToken);
         api
           .get("/member", {
             headers: { Authorization: `Bearer ${checkToken}` },
@@ -29,13 +32,20 @@ export default function MemberUpdate() {
           .catch((err) => console.log(err));
       }
     }
-  }, []);
+  }, [updateList]);
 
   const [memberInfo, setMemberInfo] = useState<IMemberInfo>();
+  const [nickname, setNickname] = useState("");
+  const [age, setAge] = useState("");
+  useEffect(() => {
+    if (memberInfo) {
+      setNickname(memberInfo.nickname);
+      setAge(memberInfo.age);
+    }
+  }, [memberInfo]);
 
   const [showNicknameDialog, setShowNicknameDialog] = useState(false);
 
-  const [selectedAge, setSelectedAge] = useState("20대");
   const [showSheet, setShowSheet] = useState(false);
 
   const toggleShowSheet = () => {
@@ -44,20 +54,35 @@ export default function MemberUpdate() {
 
   return (
     <>
-      <TopBar hasBack title="회원 정보 수정" noShadow />
+      <TopBar hasBack title="회원 정보 조회" noShadow />
       <div className="white-screen flex-col justify-start pt-[50px]">
         <div className="inner-screen">
+          <MemberUpdateCard
+            title="이메일"
+            content={memberInfo?.email!}
+            onClick={() => {
+              setShowNicknameDialog((prev) => !prev);
+            }}
+          />
           <MemberUpdateCard
             title="닉네임"
             content={memberInfo?.nickname!}
             onClick={() => {
               setShowNicknameDialog((prev) => !prev);
             }}
+            hasEdit
           />
-
           <MemberUpdateCard
             title="나이대"
             content={getAge(memberInfo?.age!)!}
+            onClick={() => {
+              setShowSheet(true);
+            }}
+            hasEdit
+          />
+          <MemberUpdateCard
+            title="성별"
+            content={memberInfo?.gender === "MALE" ? "남성" : "여성"}
             onClick={() => {
               setShowSheet(true);
             }}
@@ -74,7 +99,31 @@ export default function MemberUpdate() {
                 rightText="확인"
                 onLeftClick={() => setShowNicknameDialog((prev) => !prev)}
                 onRightClick={(data) => {
-                  console.log(data);
+                  if (data.nickname !== undefined) {
+                    api
+                      .post("/member/verification/duplicate", {
+                        type: "nickname",
+                        value: data.nickname,
+                      })
+                      .then((res) => {
+                        setNickname(data.nickname);
+                        if (res.data) {
+                          api
+                            .put(
+                              "/member/information",
+                              { age: age, nickname: data.nickname },
+                              { headers: { Authorization: `Bearer ${token}` } }
+                            )
+                            .then((res) => {
+                              alert("회원 정보가 수정되었습니다");
+                              setShowNicknameDialog((prev) => !prev);
+                              setUpdateList((prev) => prev + 1);
+                            })
+                            .catch((err) => console.log(res));
+                        }
+                      })
+                      .catch((err) => alert("중복된 닉네임입니다."));
+                  }
                 }}
                 placeholder="변경할 닉네임을 입력해주세요."
               />
@@ -88,7 +137,19 @@ export default function MemberUpdate() {
             <AgeSheet
               showSheet={showSheet}
               onClose={toggleShowSheet}
-              onSelected={setSelectedAge}
+              onSelected={(selectedAge: string) => {
+                const newAge = getConstAge(selectedAge);
+                api
+                  .put(
+                    "/member/information",
+                    { age: newAge, nickname: nickname },
+                    { headers: { Authorization: `Bearer ${token}` } }
+                  )
+                  .then((res) => setUpdateList((prev) => prev + 1))
+                  .catch((err) =>
+                    alert("오류가 발생하였습니다. 다시 시도해주세요")
+                  );
+              }}
             />
           </>
         )}

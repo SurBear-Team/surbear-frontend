bd1f9838361011abe2631ec148e125e3ac389f66
kimgorok <kjyse2@naver.com>
2024-05-04 19:10:23 +0900

    🔮설문 수정_글자 수 표시 수정

--- pages/my-survey/new-survey/components/ShortAnswerQuestion.tsx
+++ pages/my-survey/new-survey/components/ShortAnswerQuestion.tsx
@@ -15,12 +15,12 @@ export const ShortAnswerType = ({
 }: ShortAnswerProps) => {
   // 필수 답변 체크 박스
   const [isChecked, setIsChecked] = useState(false);
-  const [isLimited, setIsLimited] = useState(hasLimit);
-  const [inputValue, setInputValue] = useState(value);
+  const [isLimited, setIsLimited] = useState(hasLimit); // 수정하기로 전달받는거
+  const [inputValue, setInputValue] = useState(value ? value.toString() : "");
 
   useEffect(() => {
     if (!isLimited) {
-      setInputValue(788183);
+      setInputValue("788183");
       setCount(788183);
     }
   }, [isLimited]);
@@ -34,13 +34,19 @@ export const ShortAnswerType = ({
   };
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const newValue = parseInt(e.target.value, 10);
+    const newValue = e.target.value;
+    setInputValue(newValue); // 직접 상태에 저장하지 않고 유효성 검증을 먼저 수행
+  };
 
-    if (!isNaN(newValue)) {
-      setCount(newValue);
-      setInputValue(newValue);
+  const handleInputBlur = () => {
+    const value = parseInt(inputValue, 10);
+    if (!isNaN(value) && value >= 1) {
+      setCount(value); // 입력 값이 1 이상의 정수일 경우 상태 업데이트
+    } else {
+      setInputValue(""); // 유효하지 않은 입력을 초기화
     }
   };
+
   return (
     <div className="flex justify-between gap-2 items-center pt-4">
       <MyCheckBox
@@ -52,10 +58,11 @@ export const ShortAnswerType = ({
         <div className="sm-gray-9-text text-base">최대 글자수 제한</div>
         <input
           type="number"
-          value={inputValue === 788183 ? "" : inputValue}
+          value={inputValue === "788183" ? "" : inputValue}
           disabled={!isChecked && !isLimited}
           className="w-16 p-2 rounded-lg border-[1px] border-gray-4"
           onChange={handleInputChange}
+          onBlur={handleInputBlur}
         />
       </div>
     </div>

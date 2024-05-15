1a0b6da5fb65093842998951a9d2e2df72a06faf
kimgorok <kjyse2@naver.com>
2024-05-16 04:02:42 +0900

    ✅GPT_token post하기

--- pages/api/chatgpt.js
+++ pages/api/chatgpt.js
@@ -17,11 +17,32 @@ export default async function handler(req, res) {
           },
         }
       );
+      // response data 콘솔에 출력
+      console.log("Response:", response.data);
+      // GPT 사용량 콘솔에 출력
+      console.log("Usage:", response.data.usage);
+
+      const { prompt_tokens, completion_tokens, total_tokens } =
+        response.data.usage;
+
+      const usageData = {
+        prompt_tokens: prompt_tokens,
+        completion_tokens: completion_tokens,
+        total_tokens: total_tokens,
+      };
+
+      await axios.post("https://api.surbear.site/external/gpt", usageData, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
       const answers = response.data.choices.map(
         (choice) => choice.message.content
       );
       res.status(200).json({ answers });
     } catch (error) {
+      console.error("에러 발생", error);
       res.status(500).json({ error: error.message });
     }
   } else {
